321d2baf7124b2716b5fe4ecebb73c18efcfc9d5
Himanshu Ranjan <himanshu.red98@gmail.com>
2025-08-10 09:06:04 +0530

    remove error middleware

--- internal/account/routes.go
+++ internal/account/routes.go
@@ -1,36 +1,15 @@
 package account
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
 func InitRoutes(s *Server) {
 	router := gin.Default()
 
-	router.Use(ErrorMiddleware())
-
 	router.POST("/accounts", s.CreateAccount)
 	router.GET("/accounts/:account_id", s.GetAccount)
 	router.POST("/transactions", s.Transact)
 
 	router.Run(":8080")
-}
-
-func ErrorMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.Next()
-
-		err := c.Errors.Last()
-		if err != nil {
-			c.JSON(http.StatusBadRequest, &struct {
-				Success string `json:"success"`
-				Message string `json:"message"`
-			}{
-				Success: "false",
-				Message: err.Err.Error(),
-			})
-		}
-	}
 }
